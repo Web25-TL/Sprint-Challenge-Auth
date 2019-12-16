22976d0c1997ce6c8f7aa3b587192ef1abb1e1a8
MicahJank <MicahJank@users.noreply.github.com>
2019-12-15 21:04:32 -0600

    added a get to the base

--- api/server.js
+++ api/server.js
@@ -15,4 +15,8 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
+server.get('/', (req, res) => {
+    res.send('<h1>Welcome to the dad jokes api</h1>');
+})
+
 module.exports = server;
