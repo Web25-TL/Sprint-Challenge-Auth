5f459c9d1ffd19a512179b209100089d52be1aee
MicahJank <MicahJank@users.noreply.github.com>
2019-12-15 20:17:23 -0600

    finished testing the auth routers and added the code for the authenticate middleware

---
+++ auth/auth-router.spec.js
@@ -0,0 +1,58 @@
+const request = require('supertest');
+const server = require('../api/server.js');
+
+const db = require('../database/dbConfig.js');
+
+
+describe('auth-router testing', () => {
+
+    describe('registering a user', () => {
+        it('should return 201 OK', () => {
+            return db('users').truncate() // <-start fresh each test
+                    .then(() => {
+                        return request(server).post('/api/auth/register').send({ username: 'Micah', password: 'passness' })
+                    })
+                    .then(res => {
+                        expect(res.status).toBe(201);
+                    })
+        });
+
+        it('should return a JSON object', () => {
+            return db('users').truncate() // <-start fresh each test
+                    .then(() => {
+                        return request(server).post('/api/auth/register').send({ username: 'Micah', password: 'passness' })
+                    })
+                    .then(res => {
+                        expect(res.type).toBe('application/json');
+                    })
+        });
+    });
+
+    describe('logging in a user', () => {
+        it('should return 200 OK', () => {
+            return db('users').truncate() // <-start fresh each test
+            .then(() => { // register a user before logging in that user
+                return request(server).post('/api/auth/register').send({ username: 'Micah', password: 'passness' })
+            })
+            .then(() => {
+                return request(server).post('/api/auth/login').send({ username: 'Micah', password: 'passness' })
+            })
+            .then(res => {
+                expect(res.status).toBe(200);
+            });
+        });
+
+        it('should return a JSON object', () => {
+            return db('users').truncate() // <-start fresh each test
+            .then(() => { // register a user before logging in that user
+                return request(server).post('/api/auth/register').send({ username: 'Micah', password: 'passness' })
+            })
+            .then(() => {
+                return request(server).post('/api/auth/login').send({ username: 'Micah', password: 'passness' })
+            })
+            .then(res => {
+                expect(res.type).toBe('application/json');
+            });
+        })
+    })
+})
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,23 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../config/secrets.js');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  if(req.decodedJwt) {
+    next();
+  } else if(req.headers.authorization) {
+    let token = req.headers.authorization;
+    token.includes('Bearer ') ? token : res.status(400).json({ message: "Unable to verify authorization header type, please make sure you have the header authorization type of 'Bearer ' (with a space) before your token."});
+    token = token.slice(7, token.length).trim();
+
+    jwt.verify(token, jwtSecret, (err, decodedJwt) => {
+      if(err) {
+        res.status(400).json({ message: 'Unable to verify token', error: err });
+      } else {
+        req.decodedJwt = decodedJwt;
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
 };
